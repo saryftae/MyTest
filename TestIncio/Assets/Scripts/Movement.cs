42da1592f1a9683790a7c7241b614f09d33ebc7f
saryftae <saraiglesiaas@gmail.com>
2023-09-27 10:16:39 +0200

    Actualizacion

--- TestIncio/Assets/Scripts/Movement.cs
+++ TestIncio/Assets/Scripts/Movement.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movimiento : MonoBehaviour
+public class Movement: MonoBehaviour
 {
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
